137f2623a808c53ddde55b88196ab83c92d4f31b
Will <yzj_000@163.com>
2025-03-07 23:21:04 +0800

    Add integration tests

---
+++ src/it/java/net/will/circuitbreaker/BasicStartIT.java
@@ -0,0 +1,12 @@
+package net.will.circuitbreaker;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BasicStartIT {
+    @Test
+    public void testBasicStart() {
+        assertEquals(Integer.valueOf(1), 1);
+    }
+}
